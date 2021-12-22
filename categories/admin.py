9e9185566a6148f727b2b20e769f2510522a7f8f
sahar-avsh <saharavsh7192@gmail.com>
2021-12-22 19:49:42 +0300

    Remove categories app

--- categories/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from categories.models import Category, OfferCategory, Skill, Interest
-
-# Register your models here.
-# admin.site.register([Category, Skill, Interest, OfferCategory])
