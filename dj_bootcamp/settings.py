f52b6defc5d4989d04087881659acf903111be6a
sahar-avsh <saharavsh7192@gmail.com>
2022-01-07 14:07:27 +0300

    Updated settings

--- dj_bootcamp/settings.py
+++ dj_bootcamp/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dcl7ece)*)r^2w_cz01%*%ot_@64lsp)*&tz!))s(md!*5@i1x'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,15 +85,12 @@ TEMPLATES = [
     },
 ]
 
-
-GOOGLE_MAPS_API_KEY = os.environ.get('GOOGLE_MAPS_API_KEY')
-
 MAP_WIDGETS = {
     "GooglePointFieldWidget": (
         ("zoom", 6),
         ("markerFitZoom", 8),
     ),
-    "GOOGLE_MAP_API_KEY": GOOGLE_MAPS_API_KEY
+    "GOOGLE_MAP_API_KEY": env('GOOGLE_MAPS_API_KEY')
 }
 
 AUTHENTICATION_BACKENDS = [
@@ -139,11 +140,11 @@ WSGI_APPLICATION = 'dj_bootcamp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.mysql',
-        'NAME': os.environ.get('DATABASE_NAME'),
-        'USER': os.environ.get('DATABASE_USER'),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-        'HOST': os.environ.get('DATABASE_HOST'),
-        'PORT': os.environ.get('DATABASE_PORT')
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT')
     }
 }
 
